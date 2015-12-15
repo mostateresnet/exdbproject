c6de578e8302a641eeb8320d6cc2b7b7f2309d89
Matt <matthew.t.revell@gmail.com>
2015-12-15 10:50:28 -0600

    Adds basic Experience model, as well as changes the admin

--- exdb/admin.py
+++ exdb/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, SubType, Type, Category, Audience, Organization, Keyword, Experience, ExperienceComment
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(SubType)
+admin.site.register(Type)
+admin.site.register(Category)
+admin.site.register(Audience)
+admin.site.register(Organization)
+admin.site.register(Keyword)
+admin.site.register(Experience)
+admin.site.register(ExperienceComment)
--- exdb/models.py
+++ exdb/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class SubType(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Audience(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Experience(models.Model):
+    STATUS_TYPES = (
+        ('dr', 'Draft'),
+        ('pe', 'Pending'),
+        ('ap', 'Approval'),
+        ('de', 'Denied'),
+        ('ad', 'Approved'),
+        ('co', 'Completed'),
+        ('ca', 'Cancelled')
+    )
+
+    author = models.ForeignKey(User)
+    planners = models.ManyToManyField(User, related_name='planner_set')
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    type = models.ForeignKey(Type)
+    sub_type = models.ForeignKey(SubType)
+    goal = models.TextField(null=True, blank=True)
+    keywords = models.ManyToManyField(Keyword)
+    audience = models.ForeignKey(Audience)
+    guest = models.CharField(max_length=300, null=True, blank=True)
+    guest_office = models.CharField(max_length=300, null=True, blank=True)
+    attendance = models.IntegerField(null=True, blank=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    recognition = models.ManyToManyField(Organization)
+    status = models.CharField(max_length=2, choices=STATUS_TYPES, default=STATUS_TYPES[0][0])
+
+    def __str__(self):
+        return self.title
+
+
+class ExperienceComment(models.Model):
+    experience = models.ForeignKey(Experience, related_name='comment_set')
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=now)
+    author = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ['timestamp']
