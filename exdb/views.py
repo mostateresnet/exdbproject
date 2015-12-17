20cc3c9f5c6374de78330d5275b9f37b711e3789
tom <kroll001@live.missouristate.edu>
2015-12-17 16:37:53 -0600

    Created pending list view, base and pending template.  Also added tablesorter

--- exdb/views.py
+++ exdb/views.py
@@ -1,4 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from exdb.models import Experience
 
 class WelcomeView(TemplateView):
     template_name = 'exdb/welcome.html'
+
+class PendingApprovalQueueView(ListView):
+    template_name = 'exdb/pending.html'
+    context_object_name = "experiences"
+
+    def get_queryset(self):
+        return Experience.objects.filter(status='pe')
