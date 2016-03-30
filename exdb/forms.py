374ecb9d3229b8427291b030e459dd908cf5b025
Austin Stewart <aaaustin10@gmail.com>
2016-03-30 11:03:44 -0500

    Add translations

--- exdb/forms.py
+++ exdb/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.timezone import now
+from django.utils.translation import ugettext as _
 from django.forms import ModelForm
 from exdb.models import Experience, ExperienceComment
 
@@ -51,43 +52,43 @@ class ExperienceSubmitForm(ExperienceSaveForm):
     def clean(self):
 
         if not self.cleaned_data.get('description'):
-            raise ValidationError("A description is required")
+            raise ValidationError(_('A description is required'))
 
         if not self.cleaned_data.get('end_datetime'):
-            raise ValidationError("An end time is required")
+            raise ValidationError(_('An end time is required'))
 
         if not self.cleaned_data.get('start_datetime'):
-            raise ValidationError("A start time is required")
+            raise ValidationError(_('A start time is required'))
 
         if not self.cleaned_data.get('sub_type'):
-            raise ValidationError("The sub type field is required")
+            raise ValidationError(_('The sub type field is required'))
 
         if not self.cleaned_data.get('type'):
-            raise ValidationError("The type field is required")
+            raise ValidationError(_('The type field is required'))
 
         ex_type = self.cleaned_data.get('type')
 
         if not self.cleaned_data.get('next_approver') and ex_type.needs_verification:
-            raise ValidationError("Please select the supervisor to review this experience")
+            raise ValidationError(_('Please select the supervisor to review this experience'))
 
         if self.cleaned_data.get('start_datetime') >= self.cleaned_data.get('end_datetime'):
-            raise ValidationError("Start time must be before end time")
+            raise ValidationError(_('Start time must be before end time'))
 
         if not ex_type.needs_verification and self.cleaned_data.get('start_datetime') > self.when:
-            raise ValidationError(ex_type.name + " experiences must have happened in the past")
+            raise ValidationError(_('%(name)s experiences must have happened in the past') % {'name': ex_type.name})
 
         if ex_type.needs_verification and self.cleaned_data.get('start_datetime') < self.when:
-            raise ValidationError(ex_type.name + " events cannot happen in the past")
+            raise ValidationError(_('%(name)s events cannot happen in the past') % {'name': ex_type.name})
 
         if not ex_type.needs_verification and (not self.cleaned_data.get(
                 'attendance') or self.cleaned_data.get('attendance') < 1):
-            raise ValidationError(ex_type.name + " events must have an attendance")
+            raise ValidationError(_('%(name)s events must have an attendance') % {'name': ex_type.name})
 
         if not ex_type.needs_verification and not self.cleaned_data.get('audience'):
-            raise ValidationError(ex_type.name + " events must have an audience")
+            raise ValidationError(_('%(name)s events must have an audience') % {'name': ex_type.name})
 
         if ex_type.needs_verification and self.cleaned_data.get('attendance'):
-            raise ValidationError(ex_type.name + " events cannot have an attendance")
+            raise ValidationError(_('%(name)s events cannot have an attendance') % {'name': ex_type.name})
 
         return self.cleaned_data
 
@@ -103,13 +104,13 @@ class ExperienceConclusionForm(ModelForm):
 
     def clean(self):
         if not self.cleaned_data.get('attendance'):
-            raise ValidationError("There must be an attendance")
+            raise ValidationError(_('There must be an attendance'))
 
         if self.cleaned_data.get('attendance') < 0:
-            raise ValidationError("There cannot be a negative attendance")
+            raise ValidationError(_('There cannot be a negative attendance'))
 
         if not self.cleaned_data.get('conclusion'):
-            raise ValidationError("Please enter a conclusion")
+            raise ValidationError(_('Please enter a conclusion'))
 
 
 class ApprovalForm(ModelForm):
@@ -121,5 +122,5 @@ class ApprovalForm(ModelForm):
 
     def clean(self):
         if not self.cleaned_data.get('message'):
-            raise ValidationError("There must be a comment if the Experience is denied.")
+            raise ValidationError(_('There must be a comment if the Experience is denied.'))
         return self.cleaned_data
