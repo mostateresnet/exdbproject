14e91315193a578d54f46a25814700d12a29a05b
Shane Allgeier <shaneallgeier@gmail.com>
2016-08-10 13:48:18 -0500

    Add back the status column, remove unused tablesorter theme, add missing trans tags to search form

--- exdb/static/exdb/js/search_results.js
+++ exdb/static/exdb/js/search_results.js
@@ -1,7 +1,6 @@
 "use strict";
 $(document).ready(function () {
     $('#search-results').tablesorter({
-        theme: "bootstrap",
         widgets: ["saveSort", "columns", "filter"],
         textExtraction: "basic",
     });
