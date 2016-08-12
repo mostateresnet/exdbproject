056bced517d44d6d4028dc23f55efa70015193f9
Shane Allgeier <shaneallgeier@gmail.com>
2016-08-12 13:13:56 -0500

    Switch to new top bar style

--- exdb/static/exdb/js/base.js
+++ exdb/static/exdb/js/base.js
@@ -1,7 +1,6 @@
 'use strict';
 
 $(document).ready(function () {
-    $('button#mobile-menu-toggle').click(function () {
-        $('ul#mobile-menu').toggleClass('hide', !$('ul#mobile-menu').hasClass('hide'));
-    });
+    $(document).foundation();
 });
+
