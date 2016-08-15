fe35b250c7144a9194a5bc25b5cc9ea60c1f55b7
tom <kroll001@live.missouristate.edu>
2016-08-15 13:40:00 -0500

    Added FontAwesome, magnifying glass in search bars. Browser tests broken

--- exdb/static/exdb/js/search.js
+++ exdb/static/exdb/js/search.js
@@ -3,5 +3,9 @@ $(document).ready(function () {
     $('#search-results').tablesorter({
         widgets: ["saveSort", "columns", "filter"],
         textExtraction: "basic",
+        widgetOptions : {
+            filter_cssFilter:'fa',
+        },
     });
+
 });
