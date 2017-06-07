c4b5e4dfcfbbf2a742e8c39fdd60fb5aea5bd338
Shane Allgeier <shaneallgeier@gmail.com>
2017-06-06 19:22:10 -0500

    Add translation ability to form labels

--- exdb/forms.py
+++ exdb/forms.py
@@ -68,10 +68,10 @@ class ExperienceSaveForm(ModelForm):
         }
 
         labels = {
-            'start_datetime': 'Starting Date & Time',
-            'end_datetime': 'Ending Date & Time',
-            'next_approver': 'Supervisor',
-            'name': 'Title',
+            'start_datetime': _('Starting Date & Time'),
+            'end_datetime': _('Ending Date & Time'),
+            'next_approver': _('Supervisor'),
+            'name': _('Title'),
         }
 
     def __init__(self, *args, **kwargs):
